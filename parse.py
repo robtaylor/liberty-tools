26d925db9f0797629b0d7d8083007f6b15fcc083
Rob Taylor <robtaylor@floopily.org>
2020-12-22 13:51:33 +0000

    Parsing sees to be working now

--- parse.py
+++ parse.py
@@ -9,12 +9,57 @@
 ###############################################################################
 
 
-from arpeggio import PTNodeVisitor
+from arpeggio import PTNodeVisitor, visit_parse_tree
 from arpeggio.peg import ParserPEG
 
 
 class LibertyNodeVisitor(PTNodeVisitor):
-    pass
+    def __init__(self, *args, **kwargs):
+        super(LibertyNodeVisitor, self).__init__(*args, **kwargs)
+
+    def visit_library(self, node, children):
+        #print(f"\n\nlibrary: {children}")
+        libraryname = children[0]
+        elements = children[1:]
+        return {"library": {"name": libraryname,
+                            "elements": elements}}
+
+    def visit_libraryname(self, node, children):
+        return str(children[0])
+
+    def visit_cell(self, node, children):
+        return {"cell": {"name": children[0],
+                         "elements": children[1]}}
+
+    def visit_bus(self, node, children):
+        return {"bus", children}
+
+    def visit_simple_attribute(self, node, children):
+        return {children[0]: children[1]}
+
+    def visit_complex_attribute(self, node, children):
+        #print(f"\n\ncomplex attribute:  {children}")
+        values = []
+        if len(children) > 1:
+            values = children[1]
+        return {children[0]: values}
+
+    def visit_group(self, node, children):
+        if len(children) < 3:
+            return {"group": {"type": children[0],
+                              "elements": children[1]}}
+        else:
+            return {"group": {"type": children[0],
+                              "name": children[1],
+                              "elements": children[2]}}
+
+    def visit_valueset(self, node, children):
+        # print(f"\n\nvalueset:  {children}")
+        return children
+
+    def visit_valuetypelist(self, node, children):
+        #print(f"\n\nvaluetypelist: {children}")
+        return children
 
 
 def main(debug=False):
@@ -24,6 +69,9 @@ def main(debug=False):
     tree = parser.parse(test)
     print(tree.tree_str())
 
+    result = visit_parse_tree(tree, LibertyNodeVisitor(debug=debug))
+    #print(result)
+
 
 if __name__ == '__main__':
-    main(debug=True)
+    main(debug=False)
