279915c2db545d5438421bfa25034321398eca60
Rob Taylor <robtaylor@floopily.org>
2020-12-30 04:08:31 +0000

    First cut AST

--- parse.py
+++ parse.py
@@ -13,6 +13,7 @@ import concurrent.futures
 
 from enum import Enum
 from glob import glob
+from dataclasses import dataclass
 
 from arpeggio import PTNodeVisitor, visit_parse_tree
 from arpeggio.peg import ParserPEG
@@ -31,35 +32,34 @@ class Type(Enum):
     BOOL = "Bool"
 
 
+@dataclass
 class Value:
     type: str
 
 
+@dataclass
 class Library(Node):
     name: str
     elements: [Node]
 
 
-class Cell(Node):
-    name: str
-    values: [Node]
-
-
+@dataclass
 class Group(Node):
     name: str
     parameters: [Node]
     attributes: [Node]
 
 
-class Define(Node):
-    attribute_name: str
-    group_name: str
-    attribute_type: str
-
-
+@dataclass
 class SimpleAttribute(Node):
     name: str
     value: Value
+
+
+@dataclass
+class ComplexAttribute(Node):
+    name: str
+    values: [Value]
 
 
 class LibertyNodeVisitor(PTNodeVisitor):
@@ -70,44 +70,30 @@ class LibertyNodeVisitor(PTNodeVisitor):
         # print(f"\n\nlibrary: {children}")
         libraryname = children[0]
         elements = children[1:]
-        return {"library": {"name": libraryname,
-                            "elements": elements}}
+        return Library(libraryname, elements)
 
     def visit_libraryname(self, node, children):
         return str(children[0])
 
-    def visit_cell(self, node, children):
-        return {"cell": {"name": children[0],
-                         "elements": children[1]}}
-
-    def visit_bus(self, node, children):
-        return {"bus", children}
-
     def visit_simple_attribute(self, node, children):
-        return {children[0]: children[1]}
+        return SimpleAttribute(children[0], children[1])
 
     def visit_complex_attribute(self, node, children):
-        # print(f"\n\ncomplex attribute:  {children}")
         values = []
         if len(children) > 1:
             values = children[1]
-        return {children[0]: values}
+        return ComplexAttribute(children[0], values)
 
     def visit_group(self, node, children):
         if len(children) < 3:
-            return {"group": {"type": children[0],
-                              "elements": children[1]}}
+            return Group(children[0], [], children[1])
         else:
-            return {"group": {"type": children[0],
-                              "name": children[1],
-                              "elements": children[2]}}
+            return Group(children[0], children[1],  children[2])
 
     def visit_valueset(self, node, children):
-        # print(f"\n\nvalueset:  {children}")
         return children
 
     def visit_valuetypelist(self, node, children):
-        # print(f"\n\nvaluetypelist: {children}")
         return children
 
 
@@ -124,13 +110,14 @@ def main(debug=False, all=False):
 
     if all:
         tests = glob('tests/*.lib')
-        print(tests)
         with concurrent.futures.ThreadPoolExecutor() as executor:
-            results = executor.map(parse_file,tests)
+            results = executor.map(parse_file, tests)
+            print(results)
         return results
     else:
-        return parse_file('tests/sky130_fd_sc_hvl__ff_085C_5v50_lv1v95.lib')
+        result = parse_file('tests/sky130_fd_sc_hvl__ff_085C_5v50_lv1v95.lib')
+        print(result)
 
 
 if __name__ == '__main__':
-    main(debug=False, all=True)
+    main(debug=False, all=False)
